f43c943a92edd6c11b4481e1e361da435609a1bb
Fuji, Goro <gfuji@cpan.org>
2011-09-26 02:00:28 +0900

    Tweaks for benchmarks

--- README.md
+++ README.md
@@ -8,48 +8,18 @@ BENCHMARK
 ==================
 bench.cpp, invoked by `make bench`, is a simple benchmark.
 An example output is (scale: sec.):
-
-    size	1024
-    std::sort	0
-    std::stable_sort	0
-    timsort	0
-    size	2048
-    std::sort	0.01
-    std::stable_sort	0.02
-    timsort	0
-    size	4096
-    std::sort	0.01
-    std::stable_sort	0.02
-    timsort	0.01
-    size	8192
-    std::sort	0.02
-    std::stable_sort	0.05
-    timsort	0
-    size	16384
-    std::sort	0.05
-    std::stable_sort	0.09
-    timsort	0.02
-    size	32768
-    std::sort	0.09
-    std::stable_sort	0.26
-    timsort	0.04
-    size	65536
-    std::sort	0.21
-    std::stable_sort	0.63
-    timsort	0.08
-    size	131072
-    std::sort	0.54
-    std::stable_sort	1.48
-    timsort	0.17
-    size	262144
-    std::sort	1.08
-    std::stable_sort	4.36
-    timsort	0.31
-    size	524288
-    std::sort	2.41
-    std::stable_sort	8.44
-    timsort	0.68
-    size	1048576
-    std::sort	4.92
-    std::stable_sort	14.2
-    timsort	1.3
+    int
+    size	100000
+    std::sort	0.39
+    std::stable_sort	0.82
+    timsort	0.12
+    double
+    size	100000
+    std::sort	0.67
+    std::stable_sort	2.13
+    timsort	0.19
+    boost::rational
+    size	100000
+    std::sort	13.66
+    std::stable_sort	10.52
+    timsort	1.01
--- bench.cpp
+++ bench.cpp
@@ -2,6 +2,9 @@
 #include <algorithm>
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
+#include <boost/rational.hpp>
+
 #include <boost/timer.hpp>
 
 #include "timsort.hpp"
@@ -24,51 +27,65 @@ bool std_compare(T x, T y) {
     return x < y;
 }
 
-int main() {
-    std::srand(std::time(NULL));
-    for(int size = 1024; size <= (1024 * 1024); size *= 2) {
-        std::cerr << "size\t" << size << std::endl;
 
-        std::vector<int> a;
-        for(int i = 0; i < size; ++i) {
-            a.push_back((i+1) * 10);
-        }
+template <typename value_t>
+void bench(int const size) {
+    std::cerr << "size\t" << size << std::endl;
 
-        std::random_shuffle(a.begin(), a.end());
-
-        {
-            std::vector<int> b(a);
-            boost::timer t;
-
-            for(int i = 0; i < 100; ++i) {
-                std::sort(b.begin(), b.end(), &std_compare<int>);
-            }
-
-            std::cerr << "std::sort\t" << t.elapsed() << std::endl;
-        }
-
-
-        {
-            std::vector<int> b(a);
-            boost::timer t;
-
-            for(int i = 0; i < 100; ++i) {
-                std::stable_sort(b.begin(), b.end(), &std_compare<int>);
-            }
-
-            std::cerr << "std::stable_sort\t" << t.elapsed() << std::endl;
-        }
-
-
-        {
-            std::vector<int> b(a);
-            boost::timer t;
-
-            for(int i = 0; i < 100; ++i) {
-                timsort(b.begin(), b.end(), &compare<int>);
-            }
-
-            std::cerr << "timsort\t" << t.elapsed() << std::endl;
-        }
+    std::vector<value_t> a;
+    for(int i = 0; i < size; ++i) {
+        a.push_back((i+1) * 10);
     }
+
+    std::random_shuffle(a.begin(), a.end());
+
+    {
+        std::vector<value_t> b(a);
+        boost::timer t;
+
+        for(int i = 0; i < 100; ++i) {
+            std::sort(b.begin(), b.end(), &std_compare<value_t>);
+        }
+
+        std::cerr << "std::sort\t" << t.elapsed() << std::endl;
+    }
+
+
+    {
+        std::vector<value_t> b(a);
+        boost::timer t;
+
+        for(int i = 0; i < 100; ++i) {
+            std::stable_sort(b.begin(), b.end(), &std_compare<value_t>);
+        }
+
+        std::cerr << "std::stable_sort\t" << t.elapsed() << std::endl;
+    }
+
+
+    {
+        std::vector<value_t> b(a);
+        boost::timer t;
+
+        for(int i = 0; i < 100; ++i) {
+            timsort(b.begin(), b.end(), &compare<value_t>);
+        }
+
+        std::cerr << "timsort\t" << t.elapsed() << std::endl;
+    }
+}
+
+int main() {
+    typedef boost::rational<int> value_t;
+
+    std::srand(std::time(NULL));
+
+    std::cerr << "int" << std::endl;
+    bench<int>(100 * 1000);
+
+    std::cerr << "double" << std::endl;
+    bench<double>(100 * 1000);
+
+    std::cerr << "boost::rational" << std::endl;
+    bench< boost::rational<int> >(100 * 1000);
 }
